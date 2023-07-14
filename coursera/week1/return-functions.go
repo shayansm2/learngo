3ef6564310be2a6271089e2cf544cf5e85937ca7
shayansm2 <shayansm09@gmail.com>
2023-07-14 18:03:40 +0330

    that was a fun example

--- coursera/week1/return-functions.go
+++ coursera/week1/return-functions.go
@@ -1,7 +1,24 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func MakeDistOrigin(oX, oY float64) func(float64, float64) float64 {
 	return func(x, y float64) float64 { return math.Sqrt(math.Pow(x-oX, 2) + math.Pow(y-oY, 2)) }
+}
+
+func fA() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	fB := fA()
+	fmt.Print(fB())
+	fmt.Print(fB())
 }
