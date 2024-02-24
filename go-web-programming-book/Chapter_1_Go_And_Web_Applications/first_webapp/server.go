15086467888b2a434bb26897ab0772aa457a26f4
shayan <shayansm09@gmail.com>
2024-02-24 19:43:12 +0330

    learn a bit about interfaces in http package

--- go-web-programming-book/Chapter_1_Go_And_Web_Applications/first_webapp/server.go
+++ go-web-programming-book/Chapter_1_Go_And_Web_Applications/first_webapp/server.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 )
 
-func handler(writer http.ResponseWriter, request *http.Request) {
+func handlerFunction(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
 }
 
+type strHandler string
+
+func (this strHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, string(this))
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", handlerFunction)
+	var a strHandler = "my name is chicky"
+	http.Handle("/dev/", a)
 	http.ListenAndServe(":8080", nil)
 }
